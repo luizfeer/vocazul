6d4c0174dd6cc4539789678c0735f4a6b9c70485
Luiz Almeida <luiz.garra@live.com>
2020-07-06 16:56:37 -0300

    blog

--- components/Content.vue
+++ components/Content.vue
@@ -1,10 +1,10 @@
 <template>
-<div class="container">
+<div class="container p-0">
 
     <div class="flex justify-between mt-8 ">
-        <ul class="p-2">
+        <ul class="p-2 pr-6 pl-0">
             <li class="text-blue-5 uppercase font-medium text-xs ">veja</li>
-            <li class="text-3xl font-alternate">Como funciona</li>
+            <li class="text-2xl font-alternate">Como funciona</li>
             <li class="my-3">Entenda como o Você Azul funciona e não perca tempo, faça agora mesmo uma solicitação de crédito pessoal.</li>
             <li class="rounded-full my-2 py-2 px-4 bg-yellow uppercase w-full text-base text-white text-center font-semibold" >realizar um emprestimo</li>
         </ul>
@@ -12,7 +12,7 @@
             <li>
                 <img src="~assets/img/tablet.svg" alt="tablet">
             </li>
-            <li class="font-bold text-3xl text-blue-5">
+            <li class="font-bold text-2xl text-blue-5">
                 Baixe o app ou acesse pelo site
             </li>
              <li>
@@ -23,7 +23,7 @@
             <li>
             <img src="~assets/img/man.svg" alt="man">
             </li>
-             <li class="font-bold text-3xl text-blue-5">
+             <li class="font-bold text-2xl text-blue-5">
                 Baixe o app ou acesse pelo site
             </li>
              <li>
@@ -31,11 +31,11 @@
             </li>
         
         </ul>
-         <ul class="bg-white  p-6 m-2 rounded-lg">
+         <ul class="bg-white  p-6 m-2 mr-0 rounded-lg">
              <li>
             <img src="~assets/img/money.svg" alt="money">
              </li>
-              <li class="font-bold text-3xl text-blue-5">
+              <li class="font-bold text-2xl text-blue-5">
                 Baixe o app ou acesse pelo site
             </li>
              <li>
@@ -47,12 +47,96 @@
         <li class="text-blue-5 uppercase font-medium text-xl ">
             saiba
         </li>
-        <li class="text-2xl text-white">
+        <li class="text-2xl text-white font-bold">
             Quem somos
         </li>
         <li class="text-white mt-6">
              A Você Azul nasceu em 2018 com a ideia de facilitar a maneira das pessoas realizarem empréstimos financeiros, que normalmente são carregados de burocracia e papelas. De uma maneira inovadora, a Você Azul disponibiliza um método rápido, fácil e seguro para as pessoas realizarem seus empréstimos
         </li>
+    </ul>
+    <ul class="mt-6">
+         <li class="text-blue-5 uppercase font-medium text-xl ">
+            nosso
+        </li>
+        <li class="text-2xl font-bold">
+            Blog
+        </li>
+        <li class="grid justify-between  grid-cols-4 gap-6">
+            <ul class="min-w-60  bg-white rounded-lg">
+                <li class="">
+                    <img src="~assets/img/money.jpg" class="w-full rounded-t-lg" alt="money">
+                </li>
+                 <li class="py-4 px-6 font-bold text-2xl text-blue-5">
+                    O que é empréstimo pessoal
+                </li>
+                <li class="px-6">
+                    Muito se fala sobre empréstimo pessoal, entretanto, pouco se fala sobre o que é o empréstimo pessoal.
+                </li>
+                <li>
+
+                </li>
+                <li class="p-6 flex items-center">
+                    <span class="text-sm mr-3">Leia mais</span><img src="~assets/img/arrow.svg" alt="">
+                </li>
+            
+            </ul>
+
+            <ul class="min-w-60  bg-white rounded-lg">
+                <li class="">
+                    <img src="~assets/img/tablet.jpg" class="w-full rounded-t-lg" alt="tablet">
+                </li>
+                 <li class="py-4 px-6 font-bold text-2xl text-blue-5">
+                    Fundo DI - O que são e como funcionam
+                </li>
+                <li class="px-6">
+                    Dentre os diversos tipos de fundos de investimento disponíveis no mercado, o fundo DI está entre os mais comuns.
+                </li>
+                <li>
+
+                </li>
+                <li class="p-6 flex items-center">
+                    <span class="text-sm mr-3">Leia mais</span><img src="~assets/img/arrow.svg" alt="">
+                </li>
+            
+            </ul> 
+            <ul class="min-w-60  bg-white rounded-lg">
+                <li class="">
+                    <img src="~assets/img/note.jpg" class="w-full rounded-t-lg" alt="note">
+                </li>
+                 <li class="py-4 px-6 font-bold text-2xl text-blue-5">
+                   Quem deve declarar o IR 2020?
+                </li>
+                <li class="px-6">
+                   Governo prorroga prazo de entrega para 30 de junho. Para tirar todas as dúvidas sobre quem precisa e quem não precisa fazer...
+                </li>
+                <li>
+
+                </li>
+                <li class="p-6 flex items-center">
+                    <span class="text-sm mr-3">Leia mais</span><img src="~assets/img/arrow.svg" alt="">
+                </li>
+            
+            </ul>
+                          <ul class="min-w-60  bg-white rounded-lg">
+                <li class="">
+                    <img src="~assets/img/phone.jpg" class="w-full rounded-t-lg" alt="phone">
+                </li>
+                 <li class="py-4 px-6 font-bold text-2xl text-blue-5">
+                    Dicas para seu primeiro empréstimo
+                </li>
+                <li class="px-6">
+                    O primeiro empréstimo pessoal é aquele que nos deixa inseguros e desconfiados, entretanto, o empréstimo pessoal...
+                </li>
+                <li>
+
+                </li>
+                <li class="p-6 flex items-center">
+                    <span class="text-sm mr-3">Leia mais</span><img src="~assets/img/arrow.svg" alt="">
+                </li>
+            
+            </ul> 
+        </li>
+
     </ul>
  </div>
 </template>
