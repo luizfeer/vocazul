dd8a7bf9f95bc2408d1a7906f4c038fcf39a2f8c
Luiz Almeida <luiz.garra@live.com>
2020-07-06 12:43:41 -0300

    copy codes home

---
+++ components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+    <header class="h-140 max-w-md mx-auto lg:max-w-full md:max-w-full sm:max-w-xl lg:w-2/2 bg-cover md:pb-0 bg-auto bg-no-repeat bg-center pt-8 pb-6 bg-right-top bg-size-cover">
+      <div class="content container max-w-screen-xl m-auto h-full flex flex-col md:justify-between p-4 md:p-0">
+        <Navbar/>
+        <Center/>
+        <Simulation/>
+      </div>
+        
+    </header>      
+      
+
+
+</template>
+<script>
+
+
+import Navbar from '~/components/Navbar.vue'
+import Center from '~/components/Center.vue'
+import Simulation from '~/components/Simulation.vue'
+export default {
+  components: {
+    Navbar,
+    Center,
+    Simulation
+  }
+}
+
+</script>
+<style>
+header{
+  background: url('~assets/header.jpg');
+  
+}
+
+</style>
+
