393c4643e85284e93fc7e3dfcafb6e97ae59be18
Luiz Almeida <luiz.garra@live.com>
2020-07-07 11:20:56 -0300

    favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'teste',
+    title: 'Você Azul',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
