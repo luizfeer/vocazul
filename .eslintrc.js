e0507303bff157368ab24b5f613bbfeac030ddcb
Luiz Almeida <luiz.garra@live.com>
2020-07-17 14:23:22 -0300

    lastest

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     parser: 'babel-eslint'
   },
   extends: [
-<<<<<<< HEAD
     '@nuxtjs',
     'plugin:nuxt/recommended'
   ],
@@ -19,16 +18,4 @@ module.exports = {
     'nuxt/no-cjs-in-config': 'off',
     'no-console': 'off'
   }
-=======
-    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
-    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    'plugin:vue/essential'
-  ],
-  // required to lint *.vue files
-  plugins: [
-    'vue'
-  ],
-  // add your custom rules here
-  rules: {}
->>>>>>> 205fe8355933416993de80caedcee6f54ce29e31
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,6 @@
 ** Default: https://github.com/tailwindcss/tailwindcss/blob/master/stubs/defaultConfig.stub.js
 */
 module.exports = {
-<<<<<<< HEAD
   theme: {
     boxShadow: {
       md: '0px 4px 20px rgba(0, 0, 0, 0.03)'
@@ -238,9 +237,6 @@ module.exports = {
 
   },
 
-=======
-  theme: {},
->>>>>>> 205fe8355933416993de80caedcee6f54ce29e31
   variants: {},
   plugins: [],
   purge: {
